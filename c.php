f4096556d3463d25374a7e995332c84a86ed138f
philopater-maher <45350813+philopater-maher@users.noreply.github.com>
2019-02-23 17:28:34 +0200

    Add files via upload

--- c.php
+++ c.php
@@ -1,4 +1,15 @@
 <?php
+    function curl($url) {
+        
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+            $data = curl_exec($ch);
+            curl_close($ch);
+
+            return $data;
+        } 
 if  (isset ($_GET['city'])) {
         error_reporting(0);
         $urlContents = curl("https://samples.openweathermap.org/data/2.5/weather?q=".$_GET['city']."&appid=b6907d289e10d714a6e88b30761fae22");
