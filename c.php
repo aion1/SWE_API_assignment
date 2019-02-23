0851c144d0ab614819412e3723cda77ba2816647
AndrewEmad98 <45388962+AndrewEmad98@users.noreply.github.com>
2019-02-23 18:13:17 +0200

    Add files via upload

--- c.php
+++ c.php
@@ -146,6 +146,10 @@ else
                 echo ' the language of this country is '.$countryArray[0]['languages'][0]['name'];
                 
                 
+                echo "<br>";
+                $countrycode= $capitalArray[0]['alpha2Code'];
+                echo '<img src="https://www.countryflags.io/'.$countrycode.'/flat/64.png">';
+                
 
                 ?>
           
@@ -154,7 +158,10 @@ else
          
      </div> 
       
-     
+      <form action="f.php" method="get">
+   			<button type="submit">Delete your account</button>
+      </form>
+
     <!-- jQuery first, then Tether, then Bootstrap JS. -->
        <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
