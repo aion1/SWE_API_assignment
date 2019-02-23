3fe1e9a3c5a528bdc64aa3ddbe83a9fbd3616bf2
philopater-maher <45350813+philopater-maher@users.noreply.github.com>
2019-02-23 17:24:08 +0200

    Add files via upload

---
+++ c.php
@@ -0,0 +1,18 @@
+<?php
+if  (isset ($_GET['city'])) {
+        error_reporting(0);
+        $urlContents = curl("https://samples.openweathermap.org/data/2.5/weather?q=".$_GET['city']."&appid=b6907d289e10d714a6e88b30761fae22");
+        
+        $weatherArray = json_decode($urlContents, true);
+        
+        $weather = "The weather in ".$_GET['city']." is currently ".$weatherArray['weather'][0]['description'].".";
+        
+        $tempInFahrenheit = intval($weatherArray['main']['temp']* 9/5 - 459.67);
+        
+        $speedInMPH = intval($weatherArray['wind']['speed']*2.24);
+        
+        $weather .=" The temperature is ".$tempInFahrenheit."&deg; F with a wind speed of ".$speedInMPH." MPH.";
+        
+        
+    }
+?>
